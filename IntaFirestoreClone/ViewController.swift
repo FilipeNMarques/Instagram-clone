f01c6b636ec0c9d0b7e4975f4b8b851be7d58448
Filipe Marques <filipi.marques@gmail.com>
2021-11-04 18:48:26 -0300

    Initial Commit

---
+++ IntaFirestoreClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IntaFirestoreClone
+//
+//  Created by Filipe Marques on 04/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
