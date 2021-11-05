83976d8145c9948e56dd48dc5ff3ee8f5abcb1f2
Filipe Marques <filipi.marques@gmail.com>
2021-11-05 16:11:31 -0300

    setup tab bar images

--- IntaFirestoreClone/Controller/MaintabController.swift
+++ IntaFirestoreClone/Controller/MaintabController.swift
@@ -2,32 +2,42 @@ import UIKit
 
 class MainTabController: UITabBarController {
     
-//    MARK: - Lifecycle
+    //    MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewControllers()
-        
-
     }
     
-//    MARK: - Helpers
+    //    MARK: - Helpers
     
     func configureViewControllers() {
         
-        view.backgroundColor = .red
         
-        let feed = FeedController()
+        tabBar.scrollEdgeAppearance = tabBar.standardAppearance
+        tabBar.tintColor = .black
         
-        let search = SearchController()
+        let feed = templateNavigationController(unselectedImage: "home_unselected", selectedImage: "home_selected", rootViewControler: FeedController())
         
-        let imageSelector = ImageSelectorController()
+        let search = templateNavigationController(unselectedImage: "search_unselected", selectedImage: "search_selected", rootViewControler: SearchController())
         
-        let notifications = NotificationController()
+        let imageSelector = templateNavigationController(unselectedImage: "plus_unselected", selectedImage: "plus_selected", rootViewControler:  ImageSelectorController())
         
-        let profile = ProfileController()
+        let notifications = templateNavigationController(unselectedImage: "like_unselected", selectedImage: "like_selected", rootViewControler: NotificationController())
+        
+        let profile = templateNavigationController(unselectedImage: "profile_unselected", selectedImage: "profile_selected", rootViewControler: ProfileController())
         
         viewControllers = [feed, search, imageSelector, notifications, profile]
+    }
+    
+    func templateNavigationController(unselectedImage: String, selectedImage: String, rootViewControler: UIViewController) -> UINavigationController {
+        
+        let nav = UINavigationController(rootViewController: rootViewControler)
+        nav.tabBarItem.image = UIImage(named: unselectedImage)
+        nav.tabBarItem.selectedImage = UIImage(named: selectedImage)
+        nav.navigationBar.tintColor = .black
+        
+        return nav
     }
 }
 
