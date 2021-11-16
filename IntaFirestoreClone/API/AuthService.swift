e0c137170365f119bde7809a1d120d292ddd1b4f
Filipe Marques <filipi.marques@gmail.com>
2021-11-16 17:37:17 -0300

    feat: create upload image function

---
+++ IntaFirestoreClone/API/AuthService.swift
@@ -0,0 +1,8 @@
+//
+//  AuthService.swift
+//  IntaFirestoreClone
+//
+//  Created by Filipe Marques on 16/11/21.
+//
+
+import Foundation
---
+++ IntaFirestoreClone/API/ImageUploader.swift
@@ -0,0 +1,24 @@
+import Foundation
+import FirebaseStorage
+import UIKit
+
+
+struct ImageUploader {
+    static func uploadImage(image: UIImage, completion: @escaping(String) -> Void) {
+        guard let imageData = image.jpegData(compressionQuality: 0.75) else { return }
+        let fileName = NSUUID().uuidString
+        let ref = Storage.storage().reference(withPath: "/profile_images/\(fileName)")
+        
+        ref.putData(imageData, metadata: nil) { metadata, error in
+            if error != nil {
+                debugPrint("Unexpected error into upload image process: \(error?.localizedDescription ?? "error description")")
+                return
+            }
+            
+            ref.downloadURL { url, error in
+                guard let imageUrl = url?.absoluteString else { return }
+                completion(imageUrl)
+            }
+        }
+    }
+}
